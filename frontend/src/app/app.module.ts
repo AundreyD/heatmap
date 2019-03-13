1d8f80aa7099565466e3d445b67b76861f8116ac
Aundrey Drummond <aundreydrummond@Aundreys-MacBook-Pro.local>
2019-03-13 07:01:34 -0400

    Attempt at heatmap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from '@angular/common/http';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    LeafletModule.forRoot(),
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoiYXVuZHJleWQiLCJhIjoiY2p0NmdxOXVrMGRuZjN5bzgyaXNobDV6ciJ9.wmFHVI_WSgkT2sJ3h4WGww'})
   ],
   providers: [],
   bootstrap: [AppComponent]
